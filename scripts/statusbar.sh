3107d86c17247521a94ffbfa1816b76e0e224691
gu999 <139931102+gu999-god@users.noreply.github.com>
2023-11-15 12:23:58 -0300

    Update statusbar.sh

--- scripts/statusbar.sh
+++ scripts/statusbar.sh
@@ -1,4 +1,9 @@
-while true; do
-	xsetroot -name "$(date +"%a, %B %d %l:%M%p"| sed 's/  / /g') | $(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/') | $(uname -r) | $(free -m | awk '/Mem/{print $3}')"
-	sleep 2
-done
+#!/bin/sh
+
+DATE=$(date +"%a %d %h %H:%M")
+VOL=$( amixer get Master | awk -F'[][]' 'END{ print $2 }')
+CPUUSE=$(cat /proc/loadavg | awk '{print $1}')
+MEMUSE=$(free -h | awk '(NR==2){ print $3 }')
+TEMPCPU=$(sed 's/000$/°C/' /sys/class/hwmon/hwmon0/temp2_input)
+
+echo " CPU: $CPUUSE% | TEMP: $TEMPCPU | MEM: $MEMUSE | VOL: $VOL | $DATE"
